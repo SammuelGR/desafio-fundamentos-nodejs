2cee433196f1e37b62f1487a4c055ac2bc8cdaa0
Sammuel Reis <sammugr@hotmail.com>
2020-06-23 19:24:50 -0300

    refactors getBalance to implements reduce function
    changes the if/else condition to a reduce with switch case to optimizate the code

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -24,21 +24,31 @@ class TransactionsRepository {
 	}
 
 	public getBalance(): Balance {
-		let incomes = 0;
-		let outcomes = 0;
+		const { income, outcome } = this.transactions.reduce(
+			(accumulator: Balance, transaction: Transaction) => {
+				switch (transaction.type) {
+					case 'income':
+						accumulator.income += transaction.value;
+						break;
+					case 'outcome':
+						accumulator.outcome += transaction.value;
+						break;
+					default:
+						break;
+				}
 
-		this.transactions.forEach(transaction => {
-			if (transaction.type === 'income') incomes += transaction.value;
-			else outcomes += transaction.value;
-		});
+				return accumulator;
+			},
+			{
+				income: 0,
+				outcome: 0,
+				total: 0,
+			},
+		);
 
-		const balance = {
-			income: incomes,
-			outcome: outcomes,
-			total: incomes - outcomes,
-		};
+		const total = income - outcome;
 
-		return balance;
+		return { income, outcome, total };
 	}
 
 	public create({ title, value, type }: CreateTransactionDTIO): Transaction {
