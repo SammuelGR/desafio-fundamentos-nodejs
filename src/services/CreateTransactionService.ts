d6ff55a521da37c3c2a1f42c279627a13698ec74
Sammuel Reis <sammugr@hotmail.com>
2020-06-23 19:28:15 -0300

    creates a new valitation to transaction types
    validates the transaction type to allow only income and outcome types

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -15,6 +15,9 @@ class CreateTransactionService {
 	}
 
 	public execute({ title, value, type }: Request): Transaction {
+		if (!['income', 'outcome'].includes(type))
+			throw Error('Invalid transaction type');
+
 		const balance = this.transactionsRepository.getBalance();
 
 		if (type === 'outcome' && balance.total - value < 0)
